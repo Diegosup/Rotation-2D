3f875a6bd329f29843fa8dccfe9bc2b69a13e75c
legoa <legoa@DESKTOP-L2FK4R0>
2023-01-22 15:20:22 -0600

    Add project files.

---
+++ Clase3/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Clase3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PCT_CANVAS = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PCT_CANVAS
+            // 
+            this.PCT_CANVAS.Location = new System.Drawing.Point(12, 12);
+            this.PCT_CANVAS.Name = "PCT_CANVAS";
+            this.PCT_CANVAS.Size = new System.Drawing.Size(928, 509);
+            this.PCT_CANVAS.TabIndex = 0;
+            this.PCT_CANVAS.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 553);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 553);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(323, 26);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(514, 555);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(725, 553);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 650);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.PCT_CANVAS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PCT_CANVAS;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Clase3/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase3
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap bmp;
+        Graphics g;
+        Point a, b, c, d,z,f,h,i,j,k;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a = new Point(0, 50);
+            b = new Point(50, 100);
+            c = new Point(100, 50);
+            d = new Point(50, 0);
+
+
+            z = new Point(0, PCT_CANVAS.Height / 2);
+            f = new Point(PCT_CANVAS.Width, PCT_CANVAS.Height / 2);
+            i = new Point(PCT_CANVAS.Width / 2, PCT_CANVAS.Height);
+            h = new Point(PCT_CANVAS.Width / 2, 0);
+
+            g.DrawLine(Pens.Red, z, f);
+            g.DrawLine(Pens.Red, i, h);
+
+            info = int.Parse(textBox1.Text);
+
+            Render(a, b, info);
+            Render(b, c, info);
+            Render(c, d, info);
+            Render(d, a, info);
+
+
+            PCT_CANVAS.Refresh();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = new Point(-50, -50);
+            b = new Point(-50, 50);
+            c = new Point(50, 50);
+            d = new Point(50, -50);
+
+
+            z = new Point(0, PCT_CANVAS.Height / 2);
+            f = new Point(PCT_CANVAS.Width, PCT_CANVAS.Height / 2);
+            i = new Point(PCT_CANVAS.Width / 2, PCT_CANVAS.Height);
+            h = new Point(PCT_CANVAS.Width / 2, 0);
+
+            g.DrawLine(Pens.Red, z, f);
+            g.DrawLine(Pens.Red, i, h);
+
+            info = int.Parse(textBox1.Text);
+
+            Render(a, b, info);
+            Render(b, c, info);
+            Render(c, d, info);
+            Render(d, a, info);
+
+
+            PCT_CANVAS.Refresh();
+        }
+
+        int info;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = new Point(0, 0);
+            b = new Point(0, 100);
+            c = new Point(100, 100);
+            d = new Point(100, 0);
+
+
+            z = new Point(0, PCT_CANVAS.Height / 2);
+            f = new Point(PCT_CANVAS.Width, PCT_CANVAS.Height / 2);
+            i = new Point(PCT_CANVAS.Width/2, PCT_CANVAS.Height);
+            h = new Point(PCT_CANVAS.Width /2,0);
+
+            j = new Point(PCT_CANVAS.Width/2,PCT_CANVAS.Height/2);
+            k = new Point((PCT_CANVAS.Width/2)+150, (PCT_CANVAS.Height / 2)-150);
+
+
+            g.DrawLine(Pens.Red,z,f) ;
+            g.DrawLine(Pens.Red, i, h);
+
+            g.DrawLine(Pens.Blue, j, k);
+
+            info = int.Parse(textBox1.Text);
+
+            Render(a, b, info);
+            Render(b, c, info);
+            Render(c, d, info);
+            Render(d, a, info);
+            
+
+            
+
+
+
+            PCT_CANVAS.Refresh();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            bmp = new Bitmap(PCT_CANVAS.Width, PCT_CANVAS.Height);
+            g = Graphics.FromImage(bmp);
+            PCT_CANVAS.Image = bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Render(Point a, Point b, double radiants)
+        {
+            PointF a2, b2;
+            double angle = (radiants * Math.PI) / 180;
+            int Sx = (bmp.Width / 2);
+            int Sy = (bmp.Height / 2);
+            a2 = new PointF(Sx + a.X, Sy - a.Y);
+            b2 = new PointF(Sx + b.X, Sy - b.Y);
+            a2.X = Sx + (float)((a.X * Math.Cos(angle)) - (a.Y * Math.Sin(angle)));
+            a2.Y = Sy - (float)((a.X * Math.Sin(angle)) + (a.Y * Math.Cos(angle)));
+            b2.X = Sx + (float)((b.X * Math.Cos(angle)) - (b.Y * Math.Sin(angle)));
+            b2.Y = Sy - (float)((b.X * Math.Sin(angle)) + (b.Y * Math.Cos(angle)));
+            g.DrawLine(Pens.Gray, a2, b2);
+        }
+    }
+}
